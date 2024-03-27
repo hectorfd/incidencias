4efe8311787b6001d255700423a3dfbbbdd67ff3
61565698+hectorfd@users.noreply.github.com <61565698+hectorfd@users.noreply.github.com>
2024-03-26 21:00:26 -0500

    limpiar

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'codigo',
+        'address',
+        'phone',
+        'role',
     ];
 
     /**
